26e4a2b84e7ddd39d5465c3abaee375f1ab5fc78
Jahongir <alimovj727@gmail.com>
2025-03-21 09:13:31 +0500

    Implemented folder creation with validation

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Folder extends Model
 {
-    //
+    
+    use HasFactory;
+
+    protected $fillable = ['name', 'icon', 'parent_id', 'user_id'];
+
+    public function parent()
+    {
+        return $this->belongsTo(Folder::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Folder::class, 'parent_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
