296b4f7fecf15835e6cf8336f1f03f6a94a92b3d
Jahongir <alimovj727@gmail.com>
2025-03-21 09:15:06 +0500

    Implemented folder update functionality

--- app/Http/Requests/FolderRequest.php
+++ app/Http/Requests/FolderRequest.php
@@ -1,28 +1,22 @@
 <?php
-
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class FolderRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return Auth::check();
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'icon' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'parent_id' => 'nullable|exists:folders,id'
         ];
     }
 }
