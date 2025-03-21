2d8f4f78b521dde1bad110faf0e928db0815fbc4
Jahongir <alimovj727@gmail.com>
2025-03-21 09:11:29 +0500

    Updated migration to include foreign key for parent_id

--- database/migrations/2025_03_20_130656_create_folders_table.php
+++ database/migrations/2025_03_20_130656_create_folders_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('icon')->nullable();
+            $table->foreignId('parent_id')->nullable()->constrained('folders')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
